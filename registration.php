d452e3cd237352e5f0276746bbef5885fa7c31f0
Dobozi Andras <dbzmonty@gmail.com>
2022-01-10 11:25:35 +0100

    Initial commit 2

--- registration.php
+++ registration.php
@@ -2,20 +2,7 @@
 
     <div class="container">
       
-      <form class="py-4" action="index.php" method="POST">
-            <div class="row">
-                <div class="col">
-                    <input type="text" name="username" class="form-control" placeholder="Username">
-                </div>
-                <div class="col">
-                    <input type="text" name="useremail" class="form-control" placeholder="Email Address">
-                </div>
-                <div class="col">
-                    <input type="submit" name="submit" class="form-control btn btn-secondary" value="Add New User">
-                    <?php echo isset($error) ? "<p>Field can't be blank</p>": ''; ?>
-                </div>
-            </div>
-        </form>
+      
 
     </div>
 
